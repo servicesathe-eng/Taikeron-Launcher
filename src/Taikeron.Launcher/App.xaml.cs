068fad1a5e9db9cc610e0176620c560c5b982b0e
servicesathe-eng <servicesathe@gmail.com>
2026-09-19 01:27:51 +0800

    Launcher 0.4.6 enforce single launcher instance

--- src/Taikeron.Launcher/App.xaml.cs
+++ src/Taikeron.Launcher/App.xaml.cs
@@ -1,5 +1,32 @@
+using System.Threading;
+
 namespace Taikeron.Launcher;
 
 public partial class App : System.Windows.Application
 {
+    private Mutex? _singleInstanceMutex;
+
+    protected override void OnStartup(System.Windows.StartupEventArgs e)
+    {
+        _singleInstanceMutex = new Mutex(
+            initiallyOwned: true,
+            name: @"Local\TaikeronLauncher.SingleInstance",
+            createdNew: out var createdNew);
+
+        if (!createdNew)
+        {
+            Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(System.Windows.ExitEventArgs e)
+    {
+        try { _singleInstanceMutex?.ReleaseMutex(); } catch { }
+        _singleInstanceMutex?.Dispose();
+        _singleInstanceMutex = null;
+        base.OnExit(e);
+    }
 }
